cae756e9b44a50287c9485bbc70a70494120cac4
Mark Rutto <markkiplagatkipruttoh@gmail.com>
2021-08-17 15:41:13 +0300

    The star match (#1)
    * The star match
    
    * Finishing up on the reset game to play again

---
+++ React/react-basics/src/star-game/PlayAgain.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PlayAgain = (props) => {
+    return(
+        <div className="game-done">
+            <button onClick={props.onClick}>Play Again</button>
+        </div>
+    )
+}
+export default PlayAgain;
