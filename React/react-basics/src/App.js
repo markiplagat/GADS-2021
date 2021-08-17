d0e69754a2ea2db05f818e23eac47f0e028db02b
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-08-17 23:57:24 +0300

    PlayAgain

--- React/react-basics/src/App.js
+++ React/react-basics/src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import CardList from '../src/github-cards-app/CardList'
 import Form from './github-cards-app/Form'
 import { Component } from 'react'
-import StarGame from "./star-game";
+import Game from "./star-game";
 
 class App extends Component {
   constructor (props) {
@@ -20,7 +20,7 @@ class App extends Component {
   render () {
     return (
             <div className="App">
-              <StarGame />
+              <Game />
               {/*<Form onSubmit={this.addNewProfile}/>*/}
               {/*<CardList profiles={this.state.profiles}/>*/}
             </div>
